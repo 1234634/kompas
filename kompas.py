6df79d28db5bbeedb17bd89f4bee70d9072b8193
1234634 <49291996+1234634@users.noreply.github.com>
2020-02-07 19:39:08 +0100

    Add files via upload
    Dictionary has only one element so that code is more resilient to errors.

--- kompas.py
+++ kompas.py
@@ -8,27 +8,25 @@ class Side(Enum):
     NORTH = 0
     SOUTH = 1
 
-    
-frame = cv2.imread('images/markers2.png')
+frame = cv2.imread('images/blenderCompas.png')
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 parameters =  aruco.DetectorParameters_create()
+dict = aruco.custom_dictionary_from(1,4,aruco_dict)
 
-corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict, parameters=parameters)
 
-'''
-if(corners[1] > 0):
+if ids is not None:
     print(Side.NORTH)
 else:
     print(Side.SOUTH)
-'''
 
 
-
+frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 cv2.imshow('image',frame_markers)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
 
 
