686e4dcd848498d2929ca9eac6f1c0601990e1f0
1234634 <49291996+1234634@users.noreply.github.com>
2020-02-09 14:41:47 +0100

    Corner comparasion
    Corner comparasion added so that we can determine rotation of the aruco . That results in us knowing on which side does compas stands.

--- kompas.py
+++ kompas.py
@@ -3,29 +3,36 @@ import cv2
 from cv2 import aruco
 from enum import Enum
 
-
 class Side(Enum):
     NORTH = 0
     SOUTH = 1
 
-frame = cv2.imread('images/blenderCompas.png')
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-parameters =  aruco.DetectorParameters_create()
-dict = aruco.custom_dictionary_from(1,4,aruco_dict)
+def main():
+    frame = cv2.imread('images/blenderCompas.png')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    parameters =  aruco.DetectorParameters_create()
+    dict = aruco.custom_dictionary_from(1,4,aruco_dict)
 
-corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict, parameters=parameters)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict, parameters=parameters)
 
-if ids is not None:
-    print(Side.NORTH)
-else:
-    print(Side.SOUTH)
+    topY = corners[0][0][0][1]
+    bottomY = corners[0][0][3][1]
+
+    print (corners)
+    if topY <= bottomY:
+        print(Side.NORTH)
+    else:
+        print(Side.SOUTH)
 
 
-frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-cv2.imshow('image',frame_markers)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+    cv2.imshow('image',frame_markers)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
 
 
 
